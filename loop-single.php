dec3e8326704de71a9693dd04214fb505abb85e5
Stefan Pettersson <stefan.pettersson@lumano.se>
2013-03-25 22:03:17 +0100

    remove category, tags, author from news posts

--- loop-single.php
+++ loop-single.php
@@ -6,16 +6,6 @@
 ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<h1><?php the_title(); ?></h1>
-	<h4>Skrivet av <em><?php the_author(); ?></em> i <em><?php $category = get_the_category(); echo $category[0]->cat_name; ?></em></h4>
-	<h4>Etiketter: <em><?php
-$posttags = get_the_tags();
-if ($posttags) {
-  foreach($posttags as $tag) {
-    echo $tag->name . ','; 
-  }
-}
-?></em></h4>
-
 	<?php the_content(); 
 	if(in_category('Nyheter')):
 		
