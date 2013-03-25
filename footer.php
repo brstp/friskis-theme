de1c4e9c6fd3e2daf94eab432d7bc19938fbcfd2
Stefan Pettersson <stefan.pettersson@lumano.se>
2013-03-25 14:53:46 +0100

    disable unused social icons

--- footer.php
+++ footer.php
@@ -17,10 +17,10 @@
 				$twitterAccount 	= get_option('twitterAccount');
 				$facebookAccount	= get_option('facebookAccount');
 				
-				if(isset($facebookAccount)): ?>
+				if(strlen(trim($facebookAccount))>0): ?>
 					<a href="http://www.facebook.com/<?php echo $facebookAccount; ?>" title="Följ oss på Facebook!"><img src="<?php echo THEME_IMAGES; ?>/facebook.png" alt="facebook"></a>
 					<?php endif; ?>
-					<?php if(isset($twitterAccount)): ?>
+					<?php if(strlen(trim($twitterAccount))>0): ?>
 					<a href="http://www.twitter.com/<?php echo $twitterAccount; ?>" title="Följ oss på Twitter!"><img src="<?php echo THEME_IMAGES; ?>/twitter.png" alt="twitter"></a>
 				<?php endif; ?>
 			</div>	
