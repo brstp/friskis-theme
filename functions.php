6ac18b7c9520d137eb4a63792623e601ca5b6fd1
Stefan Pettersson <Stefan Pettersson>
2015-01-21 14:35:35 +0100

    Included code to create custom fields.

--- functions.php
+++ functions.php
@@ -878,4 +878,344 @@ function custom_register_widget_recent_comments() {
 
 add_action( 'widgets_init', 'custom_register_widget_recent_comments' );
 
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_bildspel',
+		'title' => 'Bildspel',
+		'fields' => array (
+			array (
+				'key' => 'field_1',
+				'label' => 'Bildspel',
+				'name' => 'image-slider',
+				'type' => 'repeater',
+				'instructions' => 'Bildstorlek: 1000x400px.',
+				'sub_fields' => array (
+					array (
+						'key' => 'field_2',
+						'label' => 'Bild',
+						'name' => 'image',
+						'type' => 'image',
+						'column_width' => '',
+						'save_format' => 'url',
+						'preview_size' => 'thumbnail',
+						'library' => 'all',
+					),
+					array (
+						'key' => 'field_3',
+						'label' => 'Sidlänk',
+						'name' => 'page-link',
+						'type' => 'page_link',
+						'column_width' => '',
+						'post_type' => array (
+							0 => 'all',
+						),
+						'allow_null' => 1,
+						'multiple' => 0,
+					),
+					array (
+						'key' => 'field_17',
+						'label' => 'Alt',
+						'name' => 'alt',
+						'type' => 'text',
+						'column_width' => '',
+						'default_value' => '',
+						'formatting' => 'html',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+					array (
+						'key' => 'field_18',
+						'label' => 'Title',
+						'name' => 'title',
+						'type' => 'text',
+						'column_width' => '',
+						'default_value' => '',
+						'formatting' => 'html',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+				),
+				'row_min' => 0,
+				'row_limit' => '',
+				'layout' => 'table',
+				'button_label' => 'Add Row',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-home.php',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+	register_field_group(array (
+		'id' => 'acf_h%c3%b6gerspalt',
+		'title' => 'Högerspalt',
+		'fields' => array (
+			array (
+				'key' => 'field_13',
+				'label' => 'Högerspalt',
+				'name' => 'sidebar-right',
+				'type' => 'wysiwyg',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'post',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+			array (
+				array (
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'default',
+					'order_no' => 0,
+					'group_no' => 1,
+				),
+			),
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'post',
+					'order_no' => 0,
+					'group_no' => 2,
+				),
+			),
+			array (
+				array (
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'default',
+					'order_no' => 0,
+					'group_no' => 3,
+				),
+			),
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'fs_news',
+					'order_no' => 0,
+					'group_no' => 4,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+	register_field_group(array (
+		'id' => 'acf_puffar-startsida',
+		'title' => 'Puffar startsida',
+		'fields' => array (
+			array (
+				'key' => 'field_4',
+				'label' => 'Puffar',
+				'name' => 'boxes',
+				'type' => 'repeater',
+				'instructions' => 'Bildstorlek: 200x175px',
+				'sub_fields' => array (
+					'field_5' => array (
+						'key' => 'field_5',
+						'label' => 'Bild',
+						'name' => 'image',
+						'type' => 'image',
+						'column_width' => '',
+						'save_format' => 'url',
+						'preview_size' => 'thumbnail',
+						'library' => 'all',
+					),
+					'field_6' => array (
+						'key' => 'field_6',
+						'label' => 'Rubrik',
+						'name' => 'headline',
+						'type' => 'text',
+						'column_width' => '',
+						'default_value' => '',
+						'formatting' => 'none',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+					'field_7' => array (
+						'key' => 'field_7',
+						'label' => 'Text',
+						'name' => 'text',
+						'type' => 'textarea',
+						'column_width' => '',
+						'default_value' => '',
+						'formatting' => 'br',
+						'maxlength' => '',
+						'placeholder' => '',
+						'rows' => '',
+					),
+					'field_8' => array (
+						'key' => 'field_8',
+						'label' => 'Sidlänk',
+						'name' => 'page-link',
+						'type' => 'page_link',
+						'column_width' => '',
+						'post_type' => array (
+							0 => 'all',
+						),
+						'allow_null' => 0,
+						'multiple' => 0,
+					),
+					'field_19' => array (
+						'key' => 'field_19',
+						'label' => 'Alt',
+						'name' => 'alt',
+						'type' => 'text',
+						'column_width' => '',
+						'default_value' => '',
+						'formatting' => 'html',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+					'field_20' => array (
+						'key' => 'field_20',
+						'label' => 'Title',
+						'name' => 'title',
+						'type' => 'text',
+						'column_width' => '',
+						'default_value' => '',
+						'formatting' => 'html',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+				),
+				'row_min' => 0,
+				'row_limit' => 3,
+				'layout' => 'table',
+				'button_label' => 'Add Row',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-home.php',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+
+	register_field_group(array (
+		'id' => 'acf_sidfot',
+		'title' => 'Sidfot',
+		'fields' => array (
+			array (
+				'key' => 'field_9',
+				'label' => 'Sidfot vänster',
+				'name' => 'footer-left',
+				'type' => 'wysiwyg',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+				'the_content' => 'yes',
+			),
+			array (
+				'key' => 'field_10',
+				'label' => 'Sidfot höger',
+				'name' => 'footer-right',
+				'type' => 'wysiwyg',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+				'the_content' => 'yes',
+			),
+			array (
+				'key' => 'field_11',
+				'label' => 'Twitterflöde',
+				'name' => 'twitter',
+				'type' => 'select',
+				'choices' => array (
+					'show' => 'Visa Twitter-flöde',
+					'hide' => 'Göm Twitter-flöde',
+				),
+				'default_value' => '',
+				'allow_null' => 0,
+				'multiple' => 0,
+			),
+			array (
+				'key' => 'field_12',
+				'label' => 'Twitter användarnamn',
+				'name' => 'twitter-username',
+				'type' => 'text',
+				'default_value' => '',
+				'formatting' => 'none',
+				'maxlength' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page',
+					'operator' => '==',
+					'value' => '85',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+				0 => 'the_content',
+			),
+		),
+		'menu_order' => 0,
+	));
+}
+
+
 ?>
